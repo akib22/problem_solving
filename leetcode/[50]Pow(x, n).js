cd46b5fa2826630ac8115dded5772a33c3320cbb
Akibur Rahman <48094649+akib22@users.noreply.github.com>
2022-03-13 16:38:19 +0600

    optimised recursive solution :)

--- leetcode/[50]Pow(x, n).js
+++ leetcode/[50]Pow(x, n).js
@@ -26,3 +26,18 @@ var myPow = function(x, n) {
   
   return !isNegative ? pow : (1 / pow)
 }
+
+// Solution using recuresion
+var myPow = function(x, n) {
+  if (n === 0) return 1
+  
+  if (n < 0) {
+    return 1 / myPow(x, -n)
+  }
+  
+  if (n % 2) {
+    return x * myPow(x, n - 1)
+  }
+  
+  return myPow(x * x, n / 2)
+}
