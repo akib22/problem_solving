7ef9313f4b444fd326082aaa1bb7054e8705f231
Akibur Rahman <48094649+akib22@users.noreply.github.com>
2022-03-13 16:01:05 +0600

    TLE :(

--- leetcode/[50]Pow(x, n).js
+++ leetcode/[50]Pow(x, n).js
@@ -1,3 +1,28 @@
 var myPow = function(x, n) {
   return x ** n  
 };
+
+/* GOT TIME LIMIT EXCEED */
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+  if (n === 0) return 1
+
+  const isNegative = n < 1
+  let pow = 1
+  
+  while(n) {
+    pow *= x
+
+    if (isNegative) {
+      n++
+    } else {
+      n--
+    }
+  } 
+  
+  return !isNegative ? pow : (1 / pow)
+}
