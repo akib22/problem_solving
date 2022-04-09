ba5881ddb176dc62491fe953f31ab12c5298e872
Akibur Rahman <48094649+akib22@users.noreply.github.com>
2022-04-09 18:08:40 +0600

    Update the solution using ascii code

--- leetcode/python/[709] To Lower Case.py
+++ leetcode/python/[709] To Lower Case.py
@@ -1,3 +1,25 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
       return s.lower()
+
+# Solved using ascii code
+'''
+approach:
+- check every character of string is Uppercase or not
+- if character is upper case then convert it to lowercase
+- else do nothing
+'''
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+      lowerStr: str = ''
+
+      for i in range(len(s)):
+        char: str = s[i]
+        asciiCode: int = ord(char)
+    
+        if asciiCode > 64 and asciiCode < 91:
+          lowerStr += chr(asciiCode + 32)
+        else: 
+          lowerStr += char
+      
+      return lowerStr
